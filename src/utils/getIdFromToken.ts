3ff800972c3ee8862490f4ae84c432b54de4d474
ustav <gustavobreasy@gmail.com>
2024-05-01 02:07:04 -0300

    fix: force return type to be string

--- src/utils/getIdFromToken.ts
+++ src/utils/getIdFromToken.ts
@@ -1,7 +1,7 @@
 import { Context } from "hono";
 import { decode } from "hono/jwt";
 
-export function getIdFromToken(c: Context) {
+export function getIdFromToken(c: Context): string {
     const authorizationHeader = c.req.header("Authorization")
     const token = authorizationHeader!.split(" ")[1]
 
