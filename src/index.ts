069c3952c2f6d07bdbc31ff9b35d76549a3b1e25
gustafer <gustavobreasy@gmail.com>
2024-05-04 11:13:16 -0300

    allow cors to test my app

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,18 @@
 import { Hono } from 'hono'
 import { userRoute } from './routes/users'
 import { pokemonRoute } from './routes/pokemon'
+import { cors } from 'hono/cors'
+
 
 const app = new Hono()
+app.use(cors({
+    origin: 'http://localhost:3000',
+    allowHeaders: ['X-Custom-Header', 'Upgrade-Insecure-Requests'],
+    allowMethods: ['POST', 'GET', 'OPTIONS', 'PUT'],
+    exposeHeaders: ['Content-Length', 'X-Kuma-Revision'],
+    maxAge: 600,
+    credentials: true,
+  }))
 
 app.get('/', (c) => {
     return c.json({
